06e903bad143ad98ac0604c41ccb9cb1a3ed7db5
Gene Kao <kao.gene@gmail.com>
2019-10-29 13:10:46 +0100

    add starter code template

---
+++ 00/starter_code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+The purpose of this file
+"""
+
+__author__ = "Gene Ting-Chun Kao"
+__email__ = "kao@arch.ethz.ch"
+__date__ = "2019"
+
+
+def func(a):
+    """
+    Counter-clockwise checker for vector AB to AC
+    Args:
+        a: type, description
+    Returns:
+        None
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    pass
