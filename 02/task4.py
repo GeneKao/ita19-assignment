c853c15063313ce1c30c6a7de5894e66319bf907
Gene Kao <kao.gene@gmail.com>
2019-10-30 11:05:30 +0100

    hw2 task4 code clean up

--- 02/task4.py
+++ 02/task4.py
@@ -7,7 +7,7 @@ Task 4: traversing the mesh from boundary to boundary in a "straight" line.
 
 __author__ = "Gene Ting-Chun Kao"
 __email__ = "kao@arch.ethz.ch"
-__date__ = "29.10.2019"
+__date__ = "30.10.2019"
 
 
 def traverse_straight_line(mesh, vertex):
@@ -20,38 +20,29 @@ def traverse_straight_line(mesh, vertex):
         vertices: list of int, traverse path in vertex key
     """
     vertices = [vertex]
-    count = 0
 
     while True:
-
         v = vertices.pop()
 
-        if mesh.is_vertex_on_boundary(v) and count > 0:
+        if mesh.is_vertex_on_boundary(v) and len(vertices) > 0:
             break
 
-        if count == 0:
+        next_v = -1
+        if len(vertices) == 0:
             for nbr in mesh.vertex_neighbors(v):
-
                 if mesh.is_vertex_on_boundary(nbr):
                     continue
+                next_v = nbr
                 vertices.append(v)
-                vertices.append(nbr)
-                face = mesh.halfedge[v][nbr]
-                vertices.append(mesh.face_vertex_descendant(face, nbr))
-                vertices.append(nbr)
-            print(vertices, ", count: ", count)
         else:
             v2 = vertices.pop()
             face = mesh.halfedge[v2][v]
             next_v = mesh.face_vertex_descendant(face, v)
-            vertices.append(next_v)
-            face = mesh.halfedge[v][next_v]
-            v_side = mesh.face_vertex_descendant(face, next_v)
-            vertices.append(v_side)
-            vertices.append(next_v)
-            print(vertices, ", count: ", count)
 
-        count += 1
+        vertices.append(next_v)
+        face = mesh.halfedge[v][next_v]
+        vertices.append(mesh.face_vertex_descendant(face, next_v))
+        vertices.append(next_v)
 
     vertices.pop()
     print(vertices)
@@ -59,14 +50,11 @@ def traverse_straight_line(mesh, vertex):
     return vertices
 
 
-
-
 if __name__ == '__main__':
     import os
     import compas
     from compas.datastructures import Mesh
     from compas_plotters import MeshPlotter
-
 
     FILE = os.path.join(compas.DATA, 'faces.obj')
     mesh = Mesh.from_obj(FILE)
